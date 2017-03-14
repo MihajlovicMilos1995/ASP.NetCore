405b1bfb308639db58549c289bade94a2567e85e
Milos <mihajlovicmilos16@gmail.com>
2017-03-14 21:00:54 +0100

    Start

---
+++ People.API/src/People.API/Controllers/PeopleController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace People.API.Controllers
+{
+    [Route("api/people")]
+    public class PeopleController : Controller
+    {
+        [HttpGet()]
+        public JsonResult GetPeople()
+        {
+            return new JsonResult(PeopleDataStore.Current.People);
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult GetPerson(int id)
+        {
+            return new JsonResult(
+                PeopleDataStore.Current.People.FirstOrDefault(c => c.Id == id)
+                );
+        }
+    }
+}
